664b7b321f3814a0facf422ad2cb03d1842f2401
Tiago Rodrigues <tiaguito.andre21764@gmail.com>
2021-12-12 16:38:53 +0000

    Containers things to merge

--- containers.js
+++ containers.js
@@ -6,20 +6,22 @@
 /*class armazem{
 
 }*/
-
 class tabuleiro{
     constructor(containerId, sementes){
         this.seeds=sementes;
-        //this.containerId = containerId;
-        this.elemento = document.getElementById("containerId")
+        this.elemento = document.getElementById(containerId);
     }
 
     /*Acho que aqui esta um problema */
     semear(){
-        for(let i=0; i<this.seeds; i++){
+        for(let i=0; i < this.seeds; i++){
             let semente = document.createElement("div");
             semente.classList.add("seed");
-            elemento.appendChild(semente);
+            semente.style.top = (15+Math.random(100) * 50) + "%";
+            semente.style.left = (15+Math.random(100) * 50) + "%";
+            let valueRan = "rotate(" + Math.round(Math.random(1) * 180) + "deg)"
+            semente.style.transform = valueRan;
+            this.elemento.appendChild(semente);
         }
     }
     
@@ -39,6 +41,7 @@ jogar.onclick = function() {
     jogar.addEventListener("click",start,false);
     let numCon = conSlider.value;
 
+    // Remover todas os elementos dentro do containers Up e Down
     while(contaParentUp.firstChild) {
         contaParentUp.removeChild(contaParentUp.firstChild);
     }
@@ -46,26 +49,34 @@ jogar.onclick = function() {
         contaParentDown.removeChild(contaParentDown.firstChild);
     }
 
-    let ind = 1;
+    let ind1 = 1;
+    let ind2 = numCon;
 
     for(let i = 0; i < numCon; i++){
         let container1 = document.createElement("div");
         let container2 = document.createElement("div");
-        container1.id="c"+ind;
-        ind++;
-        container2.id="c"+ind;
-        ind++;
+
+        container1.id="c"+ind1;
+        ind1++;
+        container2.id="c"+ind2;
+        ind2++;
+
         container1.classList.add("container");
         container2.classList.add("container");
-        containerUp[i] = new tabuleiro(container1.id, seeds);
-        containerDown[i] = new tabuleiro(container2.id, seeds);
+
         contaParentUp.appendChild(container1);
         contaParentDown.appendChild(container2);
+
+        containerUp[i] = new tabuleiro(container1.id, seeds);
+        containerDown[i] = new tabuleiro(container2.id, seeds);
+
     }
+
 
     for(let j = 0; j < numCon; j++){
         /*Não consigo criar as sementes */
         containerUp[j].semear();
+        containerDown[j].semear();
     }
 
     function start(){
@@ -79,26 +90,6 @@ jogar.onclick = function() {
  */
 
 /*
-Ter uma função que verifica se containerUp ou containerDown está vazia
-Se uma dessas estiver então o jogo termina e vêmos as sementes que estão no armazem
+    Ter uma função que verifica se containerUp ou containerDown está vazia
+    Se uma dessas estiver então o jogo termina e vêmos as sementes que estão no armazem
 */
-
-
-
-
-// TESTAR ESTA FUNÇAO 
-// Perguntar à como inicializar com um certo numero
-function containersload() {
-    console.log("load");
-    let conSlider = document.getElementById("containerSlider"); 
-
-    conSlider.onload = function() {
-        let numCon = conSlider.value;
-        console.log("load");
-        for(let i = 0; i < numCon; i++){
-            let container = document.createElement("div");
-            container.classList.add("container");
-            conta.appendChild(container);
-        }
-    }
-}
